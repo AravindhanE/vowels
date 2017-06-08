cf661c613e9dbbec0980e7a3c37cfb580469bdd2
AravindhanE <aravindhkarthi97@gmail.com>
2017-06-08 18:36:28 +0530

    Create vowels.java

--- vowels.java
+++ vowels.java
@@ -1,21 +1,22 @@
+import java.io.*;
 import java.util.*;
 
-class vowels{
+class vowels {
 	public static void main (String[] args) {
-	    Scanner s=new Scanner(System.in);
-	    String str=s.next();
-	    char c[]=str.toCharArray();
-	    char a[]={'a','e','i','o','u'};
-	    int b=0;
-	    for(int i=0;i<a.length;i++)
-	     { if(c[0]==a[i])
-	     {
-	         	    System.out.println("vowels");
-	         	    b=1;
-	     }
-	     } if(b==0)
-	      	    {System.out.println("consonant");}
-
-	     
+	 Scanner s=new Scanner(System.in);
+	 int n=s.nextInt();
+	  
+	 if(n>0)
+            { 
+             System.out.println("positive");
+             }
+           else if(n<0)
+              { 
+                  System.out.println("negative");
+               }
+               else
+               {
+                    System.out.println("zero");
+               }
 	}
 }
